fe92b86d974bdf4886ee713f97f90c325e42f39a
michalgalant0 <michal_galant@wp.pl>
2023-07-02 21:55:48 +0200

    Initial backend commit.

---
+++ ExpensesApp/src/test/java/com/example/ExpensesApp/ExpensesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExpensesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
